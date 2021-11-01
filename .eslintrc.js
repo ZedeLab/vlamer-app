e2b4bd281840786d224dff83704dc57196e6b50e
abelofficial <abelofficial94@gmail.com>
2021-11-01 21:41:49 +0100

    Add project code esLint and prettier rules

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react', 'react-native', 'react-hooks', 'prettier', 'unused-imports'],
+  parser: 'react-scripts/node_modules/babel-eslint',
+  env: {
+    jest: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'padded-blocks': 'off',
+    'arrow-body-style': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  globals: {
+    fetch: false,
+  },
+};
