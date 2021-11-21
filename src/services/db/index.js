f4b8d94b805f8a18bd4fc97f4aa2b8ebb2a6d49f
abelofficial <abelofficial94@gmail.com>
2021-11-21 06:28:06 +0100

    Create db support for user volt

--- src/services/db/index.js
+++ src/services/db/index.js
@@ -3,6 +3,38 @@ import firebaseApp from '../../utils/firebase';
 import { StaticData } from './models/staticData';
 import { User } from './models/user';
 import { UserConnections } from './models/UserConnections';
+import { UserVolt } from './models/UserVolt';
+
+export const getUserVolt = async (id) => {
+  const db = getFirestore(firebaseApp);
+  const userRef = collection(db, 'volts');
+  const docRef = query(userRef, where('ownerAccountId', '==', id));
+
+  try {
+    let volt;
+    const querySnapshot = await getDocs(docRef);
+    querySnapshot.forEach((doc) => {
+      volt = { id: doc.id, ...doc.data() };
+    });
+
+    return [volt, null];
+  } catch (error) {
+    return [null, error];
+  }
+};
+
+export const addNewUserVolt = async (newUserData) => {
+  const db = getFirestore(firebaseApp);
+
+  try {
+    const userVolt = new UserVolt({ ...newUserData, volt: UserVolt.GetDefaultVoltValue() });
+    console.log('userVolt: ', userVolt);
+    await setDoc(doc(db, 'volts', userVolt.getData().id), userVolt.getData());
+    return [userVolt, null];
+  } catch (error) {
+    return [null, error];
+  }
+};
 
 export const addNewUserConnection = async (newUserData) => {
   const db = getFirestore(firebaseApp);
